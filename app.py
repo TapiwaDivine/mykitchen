c7d88b5235447186646ce0f3b7250f7c536acd84
Tapiwa Divine <tchipatiko@yahoo.com>
2019-05-29 14:38:30 +0000

    templating the index page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+from pymongo import MongoClient
 from flask_bootstrap import Bootstrap
 from bson.objectid import ObjectId
 
@@ -18,14 +19,21 @@ mongo = PyMongo(app)
 
 # display card on we website
 @app.route('/')
+@app.route('/home')
+def home():
+    return render_template("index.html")
+    
 @app.route('/display_card')
 def display_card():
     return render_template("display_card.html",
                             recipes=mongo.db.recipes.find())
+                            
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('add_recipe.html')
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
-            
